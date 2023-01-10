39cb170e9562b0667f54dd61fa9ae7f9a849da0c
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-01-10 22:04:00 +0530

    Update Step1.2_Build-up_Logical_Thinking_Patterns.cpp

--- Step1.2_Build-up_Logical_Thinking_Patterns.cpp
+++ Step1.2_Build-up_Logical_Thinking_Patterns.cpp
@@ -453,7 +453,83 @@ void print19(int n){
     }
 }
 
+// Pattern-20
+// *        *
+// **      **
+// ***    ***
+// ****  ****
+// **********
+// ****  ****
+// ***    ***
+// **      **
+// *        *
 
+void print20(int n){
+    int spaces = 2*n-2;
+    for(int i = 1; i<=2*n-1; i++){
+        int stars = i;
+        if(i>n) stars = 2*n-i;
+
+        // stars
+        for(int j = 1;j<=stars;j++){
+            cout << "*";
+        }
+
+        // spaces
+        for(int j = 1; j<=spaces; j++){
+            cout << " ";
+        }
+
+        // stars 
+         for(int j = 1;j<=stars;j++){
+            cout << "*";
+        }
+        cout << endl;
+        if(i<n) spaces -=2;
+        else spaces +=2;
+    }
+}
+
+// Pattern-21
+// *****
+// *   *
+// *   *
+// *   *
+// *****
+
+void print21(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            if(i==0 || j==0 || i==n-1 || j==n-1){
+                cout << "*";
+            }
+            else cout << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Pattern-22
+// 4 4 4 4 4 4 4
+// 4 3 3 3 3 3 4
+// 4 3 2 2 2 3 4
+// 4 3 2 1 2 3 4
+// 4 3 2 2 2 3 4
+// 4 3 3 3 3 3 4
+// 4 4 4 4 4 4 4
+
+void print22(int n){
+    for(int i = 0; i<2*n-1; i++){
+        for(int j = 0;j<2*n-1;j++){
+            int top = i;
+            int left = j;
+            int right = (2*n - 2) - j;
+            int down = (2*n - 2) - i;
+            cout << (n-min(min(top,down),min(left,right)))<<" ";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
 
