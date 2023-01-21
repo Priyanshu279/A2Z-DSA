6089f01ff9cb9bbbdc4ba8d2b474b3e3575bd182
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-01-21 23:27:51 +0530

    Update Step1.5_Recursion_Basics.cpp

--- Step1.5_Recursion_Basics.cpp
+++ Step1.5_Recursion_Basics.cpp
@@ -1,13 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
  void solve(int N) {
     int sum = N * (N + 1) / 2;
-    cout<<"The sum of the first "<<N<<" numbers is: "<<sum<<endl;
+    cout <<"The sum of the first "<< N <<" numbers is: "<< sum << endl;
   }
   
  int main() {
-
     solve(5);
     solve(6);
   }
