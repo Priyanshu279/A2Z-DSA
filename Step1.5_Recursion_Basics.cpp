a956af15f93ec90582ac411e90686c04590f281e
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-01-22 22:24:18 +0530

    Update Step1.5_Recursion_Basics.cpp

--- Step1.5_Recursion_Basics.cpp
+++ Step1.5_Recursion_Basics.cpp
@@ -5,8 +5,23 @@ using namespace std;
     int sum = n * (n + 1) / 2;
     cout <<"The sum of the first "<< n <<" numbers is: "<< sum << endl;
   }
-  
+
+//Function to reverse array using recursion
+void reverseArray(int arr[], int start, int end) {
+   if (start < end) {
+      swap(arr[start], arr[end]);
+      reverseArray(arr, start + 1, end - 1);
+   }
+}
+
  int main() {
     solve(5);
     solve(6);
+  
+   int n = 5;
+   int arr[] = { 5, 4, 3, 2, 1 };
+   reverseArray(arr, 0, n - 1);
+   printArray(arr, n);
+   return 0;
+
   }
