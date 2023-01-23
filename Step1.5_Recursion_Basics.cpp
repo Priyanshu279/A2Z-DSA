2a82fdd611699f7b95065544ea94f66ae41a4ca4
Priyanshu Suryavanshi <86827550+Priyanshu279@users.noreply.github.com>
2023-01-23 23:34:23 +0530

    Update Step1.5_Recursion_Basics.cpp

--- Step1.5_Recursion_Basics.cpp
+++ Step1.5_Recursion_Basics.cpp
@@ -20,15 +20,15 @@ void reverseArray(int arr[], int start, int end) {
    }
 }
 
- int main() {
-  
+ int main()
+ { 
     solve(8);
     solve(3);
   
    int n = 5;
    int arr[] = { 5, 4, 3, 2, 1 };
-   reverseArray(arr, 0, n - 1);
+  
+  reverseArray(arr, 0, n - 1);
    printArray(arr, n);
    return 0;
-
-  }
+ }
